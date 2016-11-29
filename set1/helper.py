03b672b886e4bf6ca21e811ed5747990d6a0ae75
Florian H <Florian@gfu.net>
2016-11-29 14:43:57 +0100

    New approach. Scoring for English text works much better now.

--- set1/helper.py
+++ set1/helper.py
@@ -1,67 +1,101 @@
-import collections
-from Crypto.Util.strxor import strxor_c, strxor
+from collections import Counter
+import math
+
+def xor_string(s1, s2):
+    assert len(s1) == len(s2)
+    output = ""
+    for c1,c2 in zip(s1.decode('hex'), s2.decode('hex')):
+        output += chr(ord(c1) ^ ord(c2))
+
+    return output.encode('hex')
+
+def xor_char(s1, c1):
+    output = ""
+    for c in s1.decode('hex'):
+        output += chr(ord(c1) ^ ord(c))
+
+    return output.encode('hex')
+"""
+letterFrequency = {
+        'a':8.167, 'b':1.492, 'c':2.782, 'd':4.253,
+        'e':12.702, 'f':2.228, 'g':2.015, 'h':6.094,
+        'i':6.966, 'j':0.153, 'k':0.772, 'l':4.025,
+        'm':2.406, 'n':6.749, 'o':7.507, 'p':1.926,
+        'q':0.095, 'r':5.987, 's':6.327, 't':9.056,
+        'v':0.987, 'w':2.360, 'x':0.150, 'y':1.974,
+        'z':0.074 }
+"""
+letterFrequency = {
+    " ": 18.28846265,
+    "e": 10.26665037,
+    "t": 7.51699827,
+    "a": 6.53216702,
+    "o": 6.15957725,
+    "n": 5.71201113,
+    "i": 5.66844326,
+    "s": 5.31700534,
+    "r": 4.98790855,
+    "h": 4.97856396,
+    "l": 3.31754796,
+    "d": 3.28292310,
+    "u": 2.27579536,
+    "c": 2.23367596,
+    "m": 2.02656783,
+    "f": 1.98306716,
+    "w": 1.70389377,
+    "g": 1.62490441,
+    "p": 1.50432428,
+    "y": 1.42766662,
+    "b": 1.25888074,
+    "v": 0.79611644,
+    "k": 0.56096272,
+    "x": 0.14092016,
+    "j": 0.09752181,
+    "q": 0.08367550,
+    "z": 0.05128469,
+}
+
+def englishTextScore(text):
 
 
-def findKeysize(text, maxSize=40):
-    keysize = (1000, -1)
-#    print text
-    for i in range(2, maxSize):
-        if 2*i > len(text):
-            break
-        s1 = text[:i]
-        s2 = text[i:2*i]
-        print (s1, s2)
-        print "ok"
 
-        n_dist = float(hamming_distance(s1,s2))/float(i)
-        print (s1, s2, n_dist)
-        if n_dist < keysize[0]:
-            keysize = (n_dist, i)
+    text = text.lower()
+    letters = Counter(text.lower())
+    usedLetters = set(letters.keys())
+    allLetters = set(letterFrequency.keys())
+    to_compare = sorted(usedLetters & allLetters)
 
-    return keysize
+    vAvgFreq = [letterFrequency[x]*100 for x in to_compare]
+    vUsedFreq = [letters[x] for x in to_compare]
+    assert len(vUsedFreq) == len(vAvgFreq)
 
-def hamming_distance(s1, s2):
-    if(len(s1) != len(s2)):
-        print "Hamming distance: Stringlength doesn't match!"
-        exit()
+    dot = 0
+    magA = 0
+    magB = 0
+    for a,b in zip(vAvgFreq, vUsedFreq):
+        dot += a*b
+        magA += a*a
+        magB += b*b
 
-    slen = len(s1)
-    diff = xor_hexstream(s1,s2)
 
-    distance = 0
-    for c in diff:
-        distance += bin(ord(c)).count("1")
-    return distance
+    if magA == 0 or magB == 0:
+        return 0
+    similarity = dot/(math.sqrt(magA)*math.sqrt(magB))
+    return similarity*len(to_compare)
 
-def chunks(s, n):
-    """Produce `n`-character chunks from `s`."""
-    result = []
-    for start in range(0, len(s), n):
-        result.append(s[start:start+n])
-    return result
+def findSingleByteXorChar(text):
+    score = -1
+    output = ""
+    char = ""
+    for i in range(1,255 + 1):
+        _output = xor_char(text, chr(i))
+        _score = englishTextScore(_output.decode('hex'))
+        if _score > score:
+            score = _score
+            output = _output
+            char = chr(i)
 
-def getMostProbableEnglishString(text):
-    englishLetterFreq = {' ': 1, 'E': 12.70, 'T': 9.06, 'A': 8.17, 'O': 7.51, 'I': 6.97, 'N': 6.75, 'S': 6.33, 'H': 6.09, 'R': 5.99, 'D': 4.25, 'L': 4.03, 'C': 2.78, 'U': 2.76, 'M': 2.41, 'W': 2.36, 'F': 2.23, 'G': 2.02, 'Y': 1.97, 'P': 1.93, 'B': 1.29, 'V': 0.98, 'K': 0.77, 'J': 0.15, 'X': 0.15, 'Q': 0.10, 'Z': 0.07}
+    return (score, output, char)
 
-    bestString = ""
-    bestStringMetric = -1000
-    key = ""
 
-    for char in range(0,128):    # a-z
-        dec = strxor_c(text.decode('hex'), char)
-        declen = len(dec)
-        letters = collections.Counter(dec)
-        sum = 0
-        for letter in letters:
-            if letter.upper() in englishLetterFreq:
-                sum += (float(letters[letter])/float(declen)) * englishLetterFreq[letter.upper()]
-            else:
-                sum -= 0.4
-
-        if sum > bestStringMetric:
-            bestStringMetric = sum
-            bestString = dec
-            key = chr(char)
-
-    return bestString, bestStringMetric, key
 
