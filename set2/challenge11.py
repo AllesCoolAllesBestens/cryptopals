9bf391cf2de253c04116d4f4d18c7c62418b3e83
Florian H <AlwaysCarryABible@googlemail.com>
2016-12-01 16:16:51 +0100

    Challenge 11 fertig

--- set2/challenge11.py
+++ set2/challenge11.py
@@ -2,7 +2,15 @@ import helper
 
 plaintext = "YELLOW SUBMARINE"*16
 
+nECB = 0
+nCBC = 0
 
-for i in range(0,20):
+for i in range(0,10000):
     ciphertext = helper.encryption_oracle(plaintext)
-    print ciphertext.encode('hex')
+    if helper.checkForECBEncryption(ciphertext,16):
+        nECB += 1
+    else:
+        nCBC += 1
+
+pECB = 100*float(nECB)/(nECB+nCBC)
+print "ECB was used %f%%, CBC was used %f%%" % (pECB, 100.0 - pECB)
